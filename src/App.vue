5db99cd1d4b31162c1e74fd9bc6cfe20ba71aeac
Mikael Hadler <mikaelhadler@gmail.com>
2019-01-15 18:37:45 -0200

    - Incluído estrutura inicial de router-links para o NavBar, component responsável por renderizar todos os links do menu

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,14 @@
 <template>
   <div id="app">
     <NavBar/>
-    <nav>
-      <router-link to="/">Home</router-link>
-      <router-link to="/contact">Contato</router-link>
-    </nav>
     <router-view />
     <Footer/>
-
   </div>
 </template>
 <script>
 import NavBar from '@/components/NavBar.vue';
 import Footer from '@/components/Footer.vue';
 import Bulma from 'bulma';
-
 
 export default {
   components: {
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,10 +1,12 @@
 <template>
   <nav>
+    <router-link to="/">Home</router-link>
+    <router-link to="/contact">Contato</router-link>
   </nav>
 </template>
 <script>
 export default {
-  name: 'NavBar',
+  name: "NavBar"
 };
 </script>
 
