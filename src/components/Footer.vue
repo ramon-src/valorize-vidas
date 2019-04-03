a2be1dc83581446b79faa39ce01061d52d1413fe
Mikael Hadler <mikaelhadler@gmail.com>
2019-04-03 14:10:34 -0300

    Aplicando breakpoints para resoluções de 360x480

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,9 +3,9 @@
     <div class="container">
       <div class="footer">
         <div class="footer__social--container">
-          <img class="footer__social--logo" :src="require('@/assets/icons/facebook.svg')" />
-          <img class="footer__social--logo" :src="require('@/assets/icons/instagram.svg')" />
-          <img class="footer__social--logo" :src="require('@/assets/icons/twitter.svg')" />
+          <img class="footer__social--icon" :src="require('@/assets/icons/facebook.svg')" />
+          <img class="footer__social--icon" :src="require('@/assets/icons/instagram.svg')" />
+          <img class="footer__social--icon" :src="require('@/assets/icons/twitter.svg')" />
         </div>
         <img class="footer__logo" :src="require('@/assets/images/logo-horizontal.svg')" />
       </div>
